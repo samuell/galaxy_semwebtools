77b41eb665a9e1a0bcdaeeed3e065b4f258a0784
Samuel Lampa <samuel.lampa@gmail.com>
2012-11-16 15:56:01 +0100

    Now prints raw XML output

--- galaxy_sparqlimport.py
+++ galaxy_sparqlimport.py
@@ -1,11 +1,30 @@
+#!/usr/bin/python
+
 from optparse import OptionParser
+import urllib, sys
 
 parser = OptionParser()
-parser.add_option("-q", "--sparql_query", metavar = "SPARQL_QUERY",
-	help = "A SPARQL query to send to a sparql endpoint")
-
+parser.add_option("-q", "--sparql_query",
+	help = "A SPARQL query to send to a SPARQL endpoint")
+parser.add_option("-u", "--url",
+	help = "The URL to the SPARQL endpoint")
 (options, args) = parser.parse_args()
-sparql_query = str(options.sparql_query)
 
-print("SPARQL query:\n" + sparql_query)
+if not options.sparql_query:
+	sys.exit("You have to specify a SPARQL query! Use the -h flag to view command line options!")
+if not options.url:
+	sys.exit("You have to specify an URL! Use the -h flag to view command line options!")
+
+sparql_query = str(options.sparql_query)
+url = options.url
+
+sparql_query_url = url + "?query=" + sparql_query
+
+# Open the URL to the SPARQL endpoint
+sparql_endpoint = urllib.urlopen(sparql_query_url)
+results = sparql_endpoint.read()
+
+# Just for debugging ...
+# print("SPARQL query:\n" + sparql_query)
+print(results)
 
